9a0902a7c90cd4d7dec53d8d4a1c53c233a9d3d4
martinfacciuto <martin_facciuto@hotmail.com>
2022-10-04 01:04:30 -0300

    [+]nodemailer & dotenv

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
+import nodemailer from 'nodemailer'
+
+const {
+	APP_PORT,
+	MAIL_HOST,
+	MAIL_PORT,
+	MAIL_USERNAME,
+	MAIL_PASSWORD,
+	MAIL_FROM_ADDRESS,
+	MAIL_FROM_NAME,
+} = process.env
 
 const app = express()
 
-app.listen(3000, () => {
-	console.log('Server is running on port 3000')
+app.get('/', async (request, response) => {
+	const { to, subject, text } = request.query
+
+	const transporter = nodemailer.createTransport({
+		host: MAIL_HOST,
+		port: MAIL_PORT,
+		secure: false,
+		auth: {
+			user: MAIL_USERNAME,
+			pass: MAIL_PASSWORD,
+		},
+	})
+
+	await transporter.sendMail({
+		from: `"${MAIL_FROM_NAME}" < ${MAIL_FROM_ADDRESS}>`,
+		to,
+		subject,
+		text,
+		html: '<h1>Mailshare</h1>',
+	})
+	response.send('Email sent')
+})
+
+app.listen(APP_PORT, () => {
+	console.log(`Server is running on http://localhost:${APP_PORT}`)
 })
